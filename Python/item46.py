caddb488987065ae8301c647d65d52c23d1248bb
fmmt <41305773+fmmt@users.noreply.github.com>
2018-10-25 15:21:08 -0700

    Add files via upload

---
+++ Python/item46.py
@@ -0,0 +1,14 @@
+#1
+print("Drill #1")
+for i in range(4):
+    print(i)
+
+#2
+print("\nDrill #2")
+for i in range(3,-1,-1):
+    print(i)
+
+#3
+print("\nDrill #3")
+for i in range(8,1,-2):
+    print(i)
