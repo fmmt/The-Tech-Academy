3c9a7d30c903c3a749d4a6ba7428fb1c07f3c23b
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-02 11:33:03 -0700

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte day;
+
+            // boolean comparison using a while statement
+            Console.WriteLine("What date is Thanksgiving in 2018?");
+            day = Convert.ToByte(Console.ReadLine());
+            while (day != 22)
+            {
+                Console.WriteLine("Your answer is wrong.\nWhat date is Thanksgiving in 2018?");
+                day = Convert.ToByte(Console.ReadLine());
+            }
+            Console.WriteLine("Correct!\n");
+
+            // boolean comparison using a do while statement
+            do
+            {
+                Console.WriteLine("What date is Thanksgiving in 2018?");
+                day = Convert.ToByte(Console.ReadLine());
+            }
+            while (day != 22);
+            Console.WriteLine("Correct!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
