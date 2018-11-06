32c05c6e687a62e5854e8a61cf52809df8991748
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-06 15:59:46 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp23/ConsoleApp23/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp23
+{
+    public class Employee<T>
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "Country", "State", "City" };
+
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 1986, 1954, 1963 };
+
+            foreach (int thing2 in employee2.Things)
+            {
+                Console.WriteLine(thing2.ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
