3d141bbc793b51dc08a10f37fe94bf5707c989fc
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-05 18:29:31 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp15/ConsoleApp15/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Math
+    {
+        public void MathOperation(int num1, int num2)
+        {
+            int answer1;
+            answer1 = num1 * num1;
+            Console.WriteLine(num2);
+            Console.Read();
+        }
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math math = new Math();
+            math.MathOperation(10, 15);
+            math.MathOperation(num1: 10, num2: 15);
+        }
+
+    }
+}
