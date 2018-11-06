edcc7a9c3f3ae66987c7d278d094a9d2cf539cf0
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-06 12:15:07 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp16/ConsoleApp16/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Math
+    {
+        public static int MathOperation(int number)
+        {
+            int answer = number * number;
+            return answer;
+        }
+        public static int MathOperation(double number)
+        {
+            int answer = Convert.ToInt32(number * 100);
+            return answer;
+        }
+        public static int MathOperation(string userInput)
+        {
+            int answer;
+            try
+            {
+                answer = Convert.ToInt32(userInput);
+            }
+            catch(Exception ex)
+            {
+                answer = userInput.Length;
+            }
+
+            return answer;
+        }
+        
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math math = new Math();
+
+            int answer;
+            int answer2;
+            int answer3;
+
+            answer = Math.MathOperation(12);
+            answer2 = Math.MathOperation(63.4);
+            answer3 = Math.MathOperation("abcde");
+
+            Console.WriteLine(answer);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+            
+            Console.Read();
+        }
+    }
+}
