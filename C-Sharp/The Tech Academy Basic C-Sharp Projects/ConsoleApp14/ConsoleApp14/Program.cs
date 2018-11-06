acc046092f34d3052ebc03e4458c652ec8167dbe
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-05 18:05:24 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main()
+        {
+            int userInput;
+            Console.WriteLine("Please provide an integer.");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            int answer1, answer2, answer3;
+            answer1 = math.PowerOf2(userInput);
+            answer2 = math.PowerOf3(userInput);
+            answer3 = math.PowerOf4(userInput);
+
+            Console.WriteLine(userInput + " to the power of 2 is " + answer1 + ".");
+            Console.WriteLine(userInput + " to the power of 3 is " + answer2 + ".");
+            Console.WriteLine(userInput + " to the power of 4 is " + answer3 + ".");
+            Console.Read();
+        }
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp14/ConsoleApp14/math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    public class math
+    {
+        public static int PowerOf2(int userInput)
+        {
+            int answer;
+            answer = userInput * userInput;
+            return answer;
+        }
+
+        public static int PowerOf3(int userInput)
+        {
+            int answer;
+            answer = userInput * userInput * userInput;
+            return answer;
+        }
+
+        public static int PowerOf4(int userInput)
+        {
+            int answer;
+            answer = userInput * userInput * userInput * userInput;
+            return answer;
+        }
+
+    }
+}
