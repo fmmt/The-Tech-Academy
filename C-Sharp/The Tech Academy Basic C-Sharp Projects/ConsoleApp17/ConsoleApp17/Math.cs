2e806d8bd6e085b04aa0b7399c4b548f1f3e0a0e
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-06 12:28:11 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp17/ConsoleApp17/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Math
+    {
+        public static int MathOperation(int number1, int number2 = 1)
+        {
+            int answer;
+            answer = number1 * number2;
+            return answer;
+        }
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number1;
+            int number2;
+            int answer;
+
+            Math math = new Math();
+
+            Console.WriteLine("Please provide an integer:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide another integer if you want:");
+            try
+            {
+                number2 = Convert.ToInt32(Console.ReadLine());
+                answer = Math.MathOperation(number1, number2);
+            }
+            catch(Exception ex)
+            {
+                answer = Math.MathOperation(number1);
+            }
+
+            Console.WriteLine(answer);
+            Console.Read();
+        }
+    }
+}
