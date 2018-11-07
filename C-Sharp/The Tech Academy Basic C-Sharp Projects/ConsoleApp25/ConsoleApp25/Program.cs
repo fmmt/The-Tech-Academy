5b0be3d25db6322c34182326421796be5173e246
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-07 11:03:51 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp25/ConsoleApp25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 20.18m;
+            Console.WriteLine(number.Amount);
+            Console.Read();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
