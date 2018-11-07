4c2e4da4dcefa086d3d46cedbed4d61cbc277eca
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-07 10:45:34 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput;
+            Console.WriteLine("What day of the week is it today?");
+            userInput = Console.ReadLine().ToUpper();
+
+            // Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
+            try
+            {
+                int enumValue = Convert.ToInt32((DayOfTheWeek)Enum.Parse(typeof(DayOfTheWeek), userInput));
+                Console.WriteLine("Variable assigned to " + userInput + ": " + enumValue);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }            
+
+            Console.Read();
+        }
+
+        private enum DayOfTheWeek
+        {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        }
+    }
+}
