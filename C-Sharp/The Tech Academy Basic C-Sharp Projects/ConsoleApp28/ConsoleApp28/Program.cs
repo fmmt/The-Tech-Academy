15a775fc91ae9b3cda2f3a5cc311178a7f25cf60
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-07 16:19:42 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. Prints the current date and time to the console.
+            Console.WriteLine(DateTime.Now);
+
+            // 2. Asks the user for a number.
+            Console.WriteLine("Please provide an integer.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // 3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            DateTime datetime = DateTime.Now.AddHours(number);
+            Console.WriteLine("It will be {0} in {1} hours.", datetime, number);
+
+            Console.Read();
+        }
+    }
+}
