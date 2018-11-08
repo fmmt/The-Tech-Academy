82f719fcd2f9d7303a216cb664f50aac8b3447d6
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-08 14:42:53 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age = 0;
+            bool validAnswer = false;
+            try
+            {
+                while (!validAnswer)
+                {
+                    // 1. Ask the user for his age.
+                    Console.WriteLine("How old are you?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter your actual age.");
+                }
+                if (age <= 0)
+                {
+                    // 4. Display appropriate error messages if user enters zero or negative numbers.
+                    throw new typoException();
+                }
+
+                DateTime today = DateTime.Today;
+                DateTime yearOfBirth = today.AddYears(-age);
+                
+                // 2. Display the year user born.
+                Console.WriteLine("You were born in {0}.", yearOfBirth.ToString("yyyy"));
+                Console.Read();
+            }
+            catch(typoException)
+            {
+                Console.WriteLine("I know you are older than {0}.", age);
+            }
+            catch(Exception)
+            {
+                // 5. Display a general message if exception caused by anything else.
+                Console.WriteLine("An error occured. Please contact your System Administrator.");
+
+            }
+
+            Console.Read();
+        }
+    }
+}
+
+
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp30/ConsoleApp30/typoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    public class typoException: Exception
+    {
+        public typoException()
+            : base() { }
+    }
+}
