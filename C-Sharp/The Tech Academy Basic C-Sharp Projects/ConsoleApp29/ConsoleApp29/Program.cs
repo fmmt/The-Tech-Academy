958e6698f9d8379fee1c8f4c42b1f240411c1ac6
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-07 17:57:53 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. Create a const variable
+            const string ConstSchool = "The Tech Academy";
+
+            // 2. Create a variable using the keyword "var".
+            var city = "Portland";
+        }
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp29/ConsoleApp29/School.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class School
+    {
+        // 3. Chain two constructors together.
+        public School(string schoolName) : this(schoolName, "Portland")
+        {
+
+        }
+        public School(string schoolName, string city)
+        {
+            SchoolName = schoolName;
+            City = city;
+        }
+
+        public string SchoolName { get; set; }
+        public string City { get; set; }
+    }
+}
