bc2ca1a48c660b0bd32e84a50d73b939c29d69e0
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-07 16:07:29 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. Ask a user for a number.
+            Console.WriteLine("Please provide a number.");
+
+            // 2. Log that number to a text file.
+            string numberW = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Fumi\Desktop\log.txt", numberW);
+
+            // 3. Print the text file back to the user.
+            string numberR = File.ReadAllText(@"C:\Users\Fumi\Desktop\log.txt");
+            Console.WriteLine(numberR);
+
+            Console.Read();
+        }
+    }
+}
