37ed4a5e9fb0c860b2ba0bfa751035353b1572ce
fmmt <41305773+fmmt@users.noreply.github.com>
2018-11-08 10:27:38 -0800

    Add files via upload

---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp22/ConsoleApp22/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    public class Employee : Person
+    {
+        public string Id { get; set; }
+
+        // Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return (employee1.Id == employee2.Id);
+        }
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return !(employee1.Id == employee2.Id);
+        }
+
+    }
+}
---
+++ C-Sharp/The Tech Academy Basic C-Sharp Projects/ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee { FirstName = "Jordan", LastName = "Smith", Id = "111" };
+            Employee employee2 = new Employee { FirstName = "Marie", LastName = "Martinez", Id = "222" };
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 != employee2);
+
+            Console.Read();
+        }
+    }
+}
